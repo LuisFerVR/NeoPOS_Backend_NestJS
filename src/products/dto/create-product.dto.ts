1de900be9516151f4b237d023f230218aad33a0f
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-05-22 13:53:44 -0600

    Se definió el DTO create de product con su validación.

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,21 @@
-export class CreateProductDto {}
+import { IsInt, IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty({ message: 'El nombre del producto es obligatoria' })
+  name: string;
+
+  @IsNotEmpty({ message: 'El precio del producto es obligatoria' })
+  @IsNumber(
+    { maxDecimalPlaces: 2 },
+    { message: 'El precio del producto debe ser un número' },
+  )
+  price: number;
+
+  @IsNotEmpty({ message: 'La cantidad no puede ir vacia' })
+  @IsNumber({ maxDecimalPlaces: 0 }, { message: 'Cantidad no válida' })
+  inventory: number;
+
+  @IsNotEmpty({ message: 'La categoria es obligatoria' })
+  @IsInt({ message: 'La categoria no es válida' })
+  CategoryId: number;
+}
