bc12fb192bdddc488027017c8ef818b4f024796f
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-05-23 16:22:04 -0600

    Se hizo el CRUD a productos.

---
+++ src/products/dto/get-product.dto.ts
@@ -0,0 +1,15 @@
+import { IsNumberString, IsOptional } from 'class-validator';
+
+export class GetProductsQueryDto {
+  @IsOptional()
+  @IsNumberString({}, { message: 'La categoria debe de ser un número' })
+  category_id?: number;
+
+  @IsOptional()
+  @IsNumberString({}, { message: 'La cantidad debe de ser un número' })
+  take: number;
+
+  @IsOptional()
+  @IsNumberString({}, { message: 'La cantidad debe de ser un número' })
+  skip: number;
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,7 +3,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
-import { Repository } from 'typeorm';
+import { FindManyOptions, Repository } from 'typeorm';
 import { Category } from 'src/categories/entities/category.entity';
 
 @Injectable()
@@ -32,26 +32,70 @@ export class ProductsService {
     });
   }
 
-  findAll() {
-    return this.productRepository.find({
+  async findAll(categoryId: number | null, take: number, skip: number) {
+    const options: FindManyOptions<Product> = {
       relations: {
         category: true,
       },
       order: {
         id: 'DESC',
       },
+      take,
+      skip,
+    };
+
+    if (categoryId) {
+      options.where = {
+        category: {
+          id: categoryId,
+        },
+      };
+    }
+
+    const [products, total] =
+      await this.productRepository.findAndCount(options);
+
+    return {
+      products,
+      total,
+    };
+  }
+
+  async findOne(id: number) {
+    const product = await this.productRepository.findOne({
+      where: { id },
+      relations: { category: true },
     });
+
+    if (!product) {
+      throw new NotFoundException(`El producto con id ${id} no existe`);
+    }
+
+    return product;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const product = await this.findOne(id);
+    Object.assign(product, updateProductDto);
+
+    if (updateProductDto.categoryId) {
+      const category = await this.categoryRepository.findOneBy({
+        id: updateProductDto.categoryId,
+      });
+
+      if (!category) {
+        let errors: string[] = [];
+        errors.push('La categoría no existe');
+        throw new NotFoundException(errors);
+      }
+      product.category = category;
+    }
+    return await this.productRepository.save(product);
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
+    return 'Producto eliminado';
   }
 }
