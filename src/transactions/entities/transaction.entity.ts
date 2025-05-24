cff7c5242e3313481cae8e27265c89e8246c6746
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-05-24 17:18:39 -0600

    Se creó el resource de transactions y se definió el modelo de transaction y TransactionContents, así como el DTO de create-transaction

---
+++ src/transactions/entities/transaction.entity.ts
@@ -0,0 +1,44 @@
+import { Product } from 'src/products/entities/product.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Transaction {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  total: number;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  transactionDate: Date;
+
+  @OneToMany(
+    () => TransactionContents,
+    (transaction) => transaction.transaction,
+  )
+  contents: TransactionContents[];
+}
+
+@Entity()
+export class TransactionContents {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('int')
+  quantity: number;
+
+  @Column('decimal')
+  price: number;
+
+  @ManyToOne(() => Product, (product) => product.id, { eager: true })
+  product: Product;
+
+  @ManyToOne(() => Transaction, (transaction) => transaction.contents)
+  transaction: Transaction;
+}
