d772a4d0fc5e4e3c4d2ce20f9c33c1381c68f1e8
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-05-24 22:07:00 -0600

    CRUD transactions.

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -10,8 +10,9 @@ import {
   Transaction,
   TransactionContents,
 } from './entities/transaction.entity';
-import { Repository } from 'typeorm';
+import { Between, FindManyOptions, Repository } from 'typeorm';
 import { Product } from 'src/products/entities/product.entity';
+import { endOfDay, isValid, parseISO, startOfDay } from 'date-fns';
 
 @Injectable()
 export class TransactionsService {
@@ -28,7 +29,11 @@ export class TransactionsService {
     await this.productRepository.manager.transaction(
       async (transactionEntityManager) => {
         const transaction = new Transaction();
-        transaction.total = createTransactionDto.total;
+
+        transaction.total = createTransactionDto.contents.reduce(
+          (total, item) => total + item.price * item.quantity,
+          0,
+        );
 
         for (const contents of createTransactionDto.contents) {
           const product = await transactionEntityManager.findOneBy(Product, {
@@ -70,19 +75,67 @@ export class TransactionsService {
     return 'Venta realizada correctamente';
   }
 
-  findAll() {
-    return `This action returns all transactions`;
+  findAll(transactionDate?: string) {
+    const options: FindManyOptions<Transaction> = {
+      relations: {
+        contents: true,
+      },
+    };
+
+    if (transactionDate) {
+      const date = parseISO(transactionDate);
+      if (!isValid(date)) {
+        throw new BadRequestException('Fecha inválida');
+      }
+      const dateStar = startOfDay(date);
+      const dateEnd = endOfDay(date);
+      options.where = {
+        transactionDate: Between(dateStar, dateEnd),
+      };
+    }
+    return this.transactionRepository.find(options);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} transaction`;
+  async findOne(id: number) {
+    const transaction = await this.transactionRepository.findOne({
+      where: { id },
+      relations: {
+        contents: true,
+      },
+    });
+
+    if (!transaction) {
+      throw new NotFoundException(`Transacción no encontrada`);
+    }
+    return transaction;
   }
 
   update(id: number, updateTransactionDto: UpdateTransactionDto) {
     return `This action updates a #${id} transaction`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
+  async remove(id: number) {
+    const transaction = await this.findOne(id);
+    for (const contents of transaction.contents) {
+      const product = await this.productRepository.findOneBy({
+        id: contents.product.id,
+      });
+      if (!product) {
+        throw new NotFoundException(
+          `Producto con ID ${contents.product.id} no encontrado`,
+        );
+      }
+      product.inventory += contents.quantity;
+      await this.productRepository.save(product);
+
+      const transactionContents =
+        await this.transactionContentsRepository.findOneBy({ id: contents.id });
+      if (transactionContents) {
+        await this.transactionContentsRepository.remove(transactionContents);
+      }
+    }
+    return {
+      message: 'Venta eliminada correctamente',
+    };
   }
 }
